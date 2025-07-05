73d81dd33349eacdb313e8e55ca1068a799f1e08
DonLevi1998 <msnarvaez@uce.edu.ec>
2025-07-05 15:43:17 -0500

    feat: :sparkles: update method update products

--- domains/products/PR-products-update/controllers/UpdateProductController.js
+++ domains/products/PR-products-update/controllers/UpdateProductController.js
@@ -1,11 +1,26 @@
-const pool = require('../../models/productModel');
+import { updateProduct } from '../models/UpdateProductModel.js';
 
-// Controlador para actualizar un producto
-async function updateProduct(id, { name, description, return_date }) {
-  const query = 'UPDATE products SET name = $1, description = $2, return_date = $3 WHERE id = $4 RETURNING *';
-  const values = [name, description, return_date, id];
-  const { rows } = await pool.query(query, values);
-  return rows[0];
-}
+const updateProductController = async (req, res) => {
+  const { id } = req.params;
+  const { name, description } = req.body;
 
-module.exports = updateProduct;
+  try {
+    if (!name || !description) {
+      return res.status(400).json({ message: 'Both name and description are required' });
+    }
+
+    const updatedProduct = await updateProduct(id, name, description);
+
+    if (!updatedProduct) {
+      return res.status(404).json({ message: 'Product not found' });
+    }
+
+    res.status(200).json({ message: 'Product updated successfully', product: updatedProduct });
+  } catch (error) {
+    console.error('Error updating product:', error);
+    res.status(500).json({ message: 'Error updating product' });
+  }
+};
+
+export { updateProductController };
+
--- domains/products/PR-products-update/server.js
+++ domains/products/PR-products-update/server.js
@@ -1,34 +1,37 @@
 import express from 'express';
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import updateProductRouter from './routes/updateProductRouter.js';
-import morgan from 'morgan';
+import UpdateProductRouter from './routes/UpdateProductRouter.js';
+import morgan from 'morgan'
+
 //import swaggerUi from 'swagger-ui-express';
 //import swaggerDocument from './swagger.json' assert { type: "json" };
-
 const app = express();
 const PORT = 5003;
 
 const corsOptions = {
   origin: "http://localhost:5173",
+  // origin: "http://54.147.184.127",
   credentials: true,
   methods: "GET,POST,PUT,DELETE",
   allowedHeaders: "Content-Type,Authorization",
 };
 
 app.use(cors(corsOptions));
-app.use(morgan('dev'));
+app.use(morgan('dev'))
 app.use(express.json());
-app.use(cookieParser());
+app.use(cookieParser()); 
+
+app.use(express.json());
 
 //app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-app.get('/', (req, res) => {
-  res.send('Welcome to the Products Update Microservice');
+app.post('/', (req, res) => {
+  res.send('Welcome to the Products Microservice');
 });
 
-app.use('/api/products', updateProductRouter);
+app.use('/api/products', UpdateProductRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
-});
+}); 
