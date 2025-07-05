6be38093e91a74b6b08b97c4e597f9be187f657e
DonLevi1998 <msnarvaez@uce.edu.ec>
2025-07-05 14:48:00 -0500

    feat: :sparkles: update method delete

--- domains/products/PR-products-delete/controllers/ProductsController.js
+++ domains/products/PR-products-delete/controllers/ProductsController.js
@@ -1,20 +1,20 @@
-import * as userModel from '../models/userModel.js';
+import { deleteProduct } from '../models/ProductsModel.js';
 
-const deleteUser = async (req, res) => {
-    const { id } = req.params;  
+const deleteProductController = async (req, res) => {
+  const { id } = req.params;
 
-    try {
-        const result = await userModel.deleteUser(id);  
+  try {
+    const deletedProduct = await deleteProduct(id);
 
-        if (!result) {
-            return res.status(404).json({ message: 'User not found' });  
-        }
-
-        return res.status(200).json({ message: 'User deleted successfully', id: result.id }); 
-    } catch (error) {
-        console.error('Error deleting user:', error);
-        res.status(500).json({ message: 'Error deleting user' });
+    if (!deletedProduct) {
+      return res.status(404).json({ message: 'Product not found' });
     }
+
+    res.status(200).json({ message: 'Product deleted successfully', id: deletedProduct.id });
+  } catch (error) {
+    console.error('Error deleting product:', error);
+    res.status(500).json({ message: 'Error deleting product' });
+  }
 };
 
-export { deleteUser };
+export { deleteProductController };
--- domains/products/PR-products-read/routes/listProductsRouter.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { getProducts } from '../controllers/ListProductsController.js';
-
-const router = express.Router();
-
-router.get('/list', getProducts);
-
-export default router;
