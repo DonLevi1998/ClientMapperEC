5e6a6c372c24c21ac9ee1103970ea0e4a384f19a
DonLevi1998 <msnarvaez@uce.edu.ec>
2025-07-06 11:16:00 -0500

    feat: :sparkles: update domain category

---
+++ domains/category/CategoryCreate/DTOs/CategoryCreateDTO.cs
@@ -0,0 +1,9 @@
+namespace CaCategoryCreate.DTOs
+{
+    public class CategoryCreateDTO
+    {
+        public string NameCategory { get; set; }
+        public string? DescriptionCategory { get; set; }
+    }
+}
+
