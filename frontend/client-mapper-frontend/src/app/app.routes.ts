c481bdc0d6fc6dc26a625ea1be06fafe67cabcf9
DonLevi1998 <msnarvaez@uce.edu.ec>
2025-07-17 17:23:30 -0500

    add files from frontend

--- frontend/client-mapper-frontend/src/app/app.routes.ts
+++ frontend/client-mapper-frontend/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { LoginComponent } from './login/login.component';
+import { CreateUserComponent } from './create-user/create-user.component';
+import { MainMenuComponent } from './main-menu/main-menu.component';
+
+export const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'create-user', component: CreateUserComponent },
+  { path: 'main-menu', component: MainMenuComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+];
